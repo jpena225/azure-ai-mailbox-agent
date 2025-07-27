52aa1a3012989f5bd863b44ddc85e9743dfd5d21
John Pena <johnpenaii@microsoft.com>
2025-07-26 18:55:56 -0700

    made a chhange

--- web_agent.py
+++ web_agent.py
@@ -142,7 +142,7 @@ def health():
 
 if __name__ == '__main__':
     # For local development
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 else:
     # For production (Gunicorn will handle this)
     pass
